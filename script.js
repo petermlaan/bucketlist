753577af54ebf03760ac6474a54f98d8136def0c
Peter Laan <plaan70@gmail.com>
2025-01-16 11:51:50 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const bucket = [];
-const btnSubmit = document.querySelector("#bucketForm button");
-btnSubmit.addEventListener("click", eventAdd);
+document.querySelector("#btnSubmit").addEventListener("click", eventAdd);
 
-function eventAdd() {
+function eventAdd(e) {
     console.log("click event");
+    console.log(e);
+    e.preventDefault();
     const name = document.querySelector("#activityName").value;
     const category = document.querySelector("#activityCategory").value;
     const activity = {
@@ -24,16 +25,33 @@ function drawBucket() {
 
 function addActivity(activity) {
     const hBucket = document.querySelector("#bucketList");
+
     const hActivity = document.createElement("div");
-    hActivity.innerHTML = 
-        activity.name + " " + activity.category 
-        + "<input type='checkbox' onclick='eventDone(this)' value='" + activity.done + "'></input>" 
-        + "<button onclick='eventRemove(this)'>Ta bort</button>";
+
+    const hSpan = document.createElement("span");
+    hSpan.innerHTML =
+        activity.name + " " + activity.category;
+    hActivity.appendChild(hSpan);
+
+    const hDone = document.createElement("input");
+    hDone.type = "checkbox";
+    hDone.addEventListener("click", eventDone);
+    hActivity.appendChild(hDone);
+
+    const hRemove = document.createElement("button");
+    hRemove.type = "submit";
+    hRemove.innerHTML = "Ta bort";
+    hRemove.value = "remove";
+    hRemove.addEventListener("click", eventRemove);
+    hActivity.appendChild(hRemove);
+
     hBucket.appendChild(hActivity);
 }
 
 function eventRemove(e) {
+    e.preventDefault();
     console.log(e);
+    e.target.parentElement.remove();
 }
 
 function eventDone(e) {
